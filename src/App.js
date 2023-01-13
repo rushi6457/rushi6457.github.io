c204f025710ea4f47cbc00f7c25abd7c1c6b0530
rushi6457 <rushiypatil@gmail.com>
2023-01-13 16:24:19 +0530

    portfolio 2

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Routes/Home';
 import About from './Routes/About';
-import Contact from './Routes/Contact';
 import Navbar from './Components/Navbar';
-import HeroImage from './Components/HeroImage';
 import Skills from './Routes/Skills';
 import { useEffect, useState } from 'react';
 import GitCalender from './Routes/GitCalender'
